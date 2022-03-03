a6cff215135bad7d269c974cd769d007820c6b79
2018023172 <yuyu980701@naver.com>
2022-03-03 10:02:08 +0900

    Initial Commit

---
+++ Test_Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Test_Project
+//
+//  Created by 김태윤 on 2022/03/03.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
